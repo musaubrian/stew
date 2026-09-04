0e071af0fd8a954bf4c4d7e9f2a7e4dd4ec629a4
musaubrian <musaubrian15@gmail.com>
2026-09-04 16:57:23 +0300

    Adds `clean` command to clear artifacts

--- src/main.zig
+++ src/main.zig
@@ -6,19 +6,23 @@ comptime {
     _ = @import("Recipe.zig");
 }
 
+const mem = std.mem;
+
 const Command = enum {
     version,
     wp,
     check,
+    clean,
     help,
     @"-h",
 };
 
 pub fn main(init: std.process.Init) !void {
     const arena = init.arena.allocator();
-    const gpa = init.gpa;
     const io = init.io;
     var verbose = false;
+
+    const trash_dir_path = ".trash";
 
     var args = init.minimal.args.toSlice(arena) catch |err| switch (err) {
         error.OutOfMemory => |oom| fatal.oom(oom),
@@ -26,14 +30,24 @@ pub fn main(init: std.process.Init) !void {
     };
 
     var recipe: Recipe = .init();
+    try recipe.dir(io, trash_dir_path, verbose, .create);
+    // should we automatically remove the trash dir after run
+    // or let the user remove it with 'clean'?
 
     for (args) |arg| {
-        if (std.mem.eql(u8, arg, "-v")) verbose = true;
+        if (mem.eql(u8, arg, "-v")) verbose = true;
     }
 
-    if (args.len <= 2) {
-        try recipe.loadAndParse(io, arena);
-        try recipe.execute(io, arena, verbose);
+    {
+        if (args.len == 1) { // stew
+            try recipe.loadAndParse(io, arena);
+            try recipe.execute(io, arena, trash_dir_path, verbose);
+        }
+
+        if (args.len <= 2 and verbose) { // != stew -v
+            try recipe.loadAndParse(io, arena);
+            try recipe.execute(io, arena, trash_dir_path, verbose);
+        }
     }
 
     const cmd = std.meta.stringToEnum(Command, args[1]) orelse {
@@ -48,28 +62,33 @@ pub fn main(init: std.process.Init) !void {
         },
         .help, .@"-h" => usage(),
         .check => try recipe.loadAndParse(io, arena),
+        .clean => try recipe.dir(io, trash_dir_path, verbose, .destroy),
         .wp => wp: {
             args = args[2..]; // stew wp | ...
             if (args.len == 0) fatal.fmt("Expected, 'list' or '<workspace>'", .{});
             try recipe.loadAndParse(io, arena);
 
-            if (std.mem.eql(u8, args[0], "list")) {
-                for (recipe.steps.items, 0..) |wp, idx| {
+            if (mem.eql(u8, args[0], "list")) {
+                for (recipe.workspaces.items, 0..) |wp, idx| {
                     if (idx == 0) {
                         std.debug.print(".\n", .{});
                         continue;
                     }
 
-                    const bar = if (idx != recipe.steps.items.len - 1) "├─" else "└─";
-                    std.debug.print("{s} {s} (cmds: {d})\n", .{ bar, wp.name, wp.commands.items.len });
+                    const bar = if (idx != recipe.workspaces.items.len - 1) "├─" else "└─";
+                    std.debug.print(
+                        "{s} {s} (cmds: {d})\n",
+                        .{ bar, wp.name, wp.commands.items.len },
+                    );
                 }
             } else {
                 const workspace = args[0];
-                for (recipe.steps.items) |wp| {
-                    if (std.mem.eql(u8, wp.name, workspace)) {
-                        try Recipe.executeWp(io, arena, wp, verbose);
-                        break :wp;
+                if (mem.eql(u8, workspace, Recipe.ROOT_WP)) fatal.fmt("Use 'stew' instead", .{});
 
+                for (recipe.workspaces.items) |wp| {
+                    if (mem.eql(u8, wp.name, workspace)) {
+                        try Recipe.executeWp(io, arena, wp, trash_dir_path, verbose);
+                        break :wp;
                     }
                 } else {
                     fatal.fmt("No workspace name {q} found", .{workspace});
@@ -77,8 +96,6 @@ pub fn main(init: std.process.Init) !void {
             }
         },
     }
-
-    _ = gpa;
 }
 
 fn usage() noreturn {
@@ -88,6 +105,7 @@ fn usage() noreturn {
         \\Commands:
         \\  wp          Interact with workspaces
         \\  check       Checks recipe file for errors
+        \\  clean       Removes non-user artifacts created (e.g .trash dir)
         \\
         \\  version     Print version number and exit
         \\  help, -h    Print this help message and exit
